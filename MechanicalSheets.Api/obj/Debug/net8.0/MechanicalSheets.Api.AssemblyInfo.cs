362a9991cc174f503a7c698203a44919d76435f4
Samuel Soliman <samuel.soliman.cs@gmail.com>
2026-04-30 08:53:53 +0200

    Update assembly version and regenerate binaries

--- MechanicalSheets.Api/obj/Debug/net8.0/MechanicalSheets.Api.AssemblyInfo.cs
+++ MechanicalSheets.Api/obj/Debug/net8.0/MechanicalSheets.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicalSheets.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc8ae994c1f53e82b9a31dd0801cfc281b4f1ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89cd6fad0bc956133e201f6f666e27348d573be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicalSheets.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicalSheets.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
